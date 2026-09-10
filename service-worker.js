2c38864b5da47a3e8f80a1e2c51da685401d0dae
arnandofirhan <arnandofp@gmail.com>
2026-09-10 14:39:14 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -28,7 +28,7 @@
  * bukan terus memakai versi lama dari cache.
  * ======================================================================
  */
-var CACHE_VERSION = 'so-shell-v35';
+var CACHE_VERSION = 'so-shell-v36';
 var SHELL_ASSETS = [
   './index.html',
   './Stylesheet.html',
